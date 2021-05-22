96e1ba05d37b789bd212cac405044a31f1524fe6
zzak00 <abderrazzak.bajjou@um6p.ma>
2021-05-22 16:45:31 +0100

    new m

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
 from flask import Flask, render_template, url_for, request
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+import string
+import csv
+import numpy as np
+from nltk.tokenize import word_tokenize
+from sklearn.metrics import classification_report
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+from string import punctuation
+
+
+def preprocess(corpus):
+    """Process tweet function.
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    words=[]
+    stemmer = PorterStemmer()
+    for i in corpus:
+
+        word_tokens = [word.lower() for word in word_tokenize(i)]
+        t=""
+        for word in word_tokens:
+            if(not word in set(stopwords.words('english')) and  not word in string.punctuation and word.isalpha()):
+                t+=stemmer.stem(word.lower())+" "
+        words.append(t)
+    # tokenize tweets
+
+    return words
+
+
 # from sklearn.externals import joblib
 app = Flask(__name__)
 
@@ -19,20 +56,20 @@ def predict():
     # Features and Labels
     df_x = df_data['CONTENT']
     df_y = df_data.CLASS
-    # Extract the features with countVectorizer
-    corpus = df_x
-    cv = TfidfVectorizer()
-    X1 = cv.fit_transform(corpus)
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X1, df_y, test_size=0.2, random_state=2)
+    # Extract the features with TFIDFVectorizer
+    corpus = preprocess(df_x)
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    X_train, X_test, y_train, y_test = train_test_split(X, df_y, test_size=0.40, random_state=42)
     # LOGISTIC REGRESSION
-    clf=LogisticRegression(random_state=0)
+    clf =  LogisticRegression(random_state=0)
     clf.fit(X_train, y_train)
     clf.score(X_test, y_test)
+    print(clf.score(X_test, y_test))
     if request.method == 'POST':
         comment = request.form['comment']
         data = [comment]
-        vect = cv.transform(data).toarray()
+        vect = vectorizer.transform(data).toarray()
         my_prediction = clf.predict(vect)
     return render_template('result.html', prediction=my_prediction)
 
